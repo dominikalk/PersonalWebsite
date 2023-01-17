58151d4ba7ee00ca624f0c2f3e09ac6e6d9f728b
Dominik Alkhovik <dominik.alk@hotmail.com>
2023-01-17 23:52:38 +0000

    add links to contact

--- components/tabs/ContactTab.tsx
+++ components/tabs/ContactTab.tsx
@@ -14,26 +14,46 @@ const ContactTab = ({ onClose, className }: ContactTabProps) => {
       className={`tab-animation ${className}`}
     >
       <div className="flex justify-center py-14 gap-6">
-        <ToolTipIcon
-          icon="/icons/envelope-white.svg"
-          label="Email"
-          size="large"
-        />
-        <ToolTipIcon
-          icon="/icons/linkedin-white.svg"
-          label="LinkedIn"
-          size="large"
-        />
-        <ToolTipIcon
-          icon="/icons/github-white.svg"
-          label="GitHub"
-          size="large"
-        />
-        <ToolTipIcon
-          icon="/icons/itch-io-white.svg"
-          label="Itch.io"
-          size="large"
-        />
+        <a
+          href="mailto:dominik.alk@hotmail.com"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <ToolTipIcon
+            icon="/icons/envelope-white.svg"
+            label="Email"
+            size="large"
+          />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/dominik-alkhovik-052897243/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <ToolTipIcon
+            icon="/icons/linkedin-white.svg"
+            label="LinkedIn"
+            size="large"
+          />
+        </a>
+        <a
+          href="https://github.com/dominikalk"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <ToolTipIcon
+            icon="/icons/github-white.svg"
+            label="GitHub"
+            size="large"
+          />
+        </a>
+        <a href="https://dominikalk.itch.io" target="_blank" rel="noreferrer">
+          <ToolTipIcon
+            icon="/icons/itch-io-white.svg"
+            label="Itch.io"
+            size="large"
+          />
+        </a>
       </div>
     </Tab>
   );
